2cbe9fafc8a848424e1425bbb15b392c7be3b969
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-11-27 18:05:47 -0500

    Create servicioController

---
+++ app/Http/Controllers/ServicioController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ServicioController extends Controller
+{
+    
+
+    public function index()
+    {
+        // return Servicio::orderBy('id', 'desc')->paginate(9);
+    }
+
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    
+    public function edit()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+}
